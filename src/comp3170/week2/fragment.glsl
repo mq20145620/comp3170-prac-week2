b49da1574769d916594e8c4c59880a484ed26d4f
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-03-09 14:45:21 +1100

    Updated to OpenGL 4.1

--- src/comp3170/week2/fragment.glsl
+++ src/comp3170/week2/fragment.glsl
@@ -1,7 +1,11 @@
+#version 410
+
 uniform vec3 u_colour;	/* colour as a 3D vector (r,g,b) */
+
+layout(location = 0) out vec4 color;
 
 void main() {
 	/* set the fragement (pixel) colour */
-    gl_FragColor = vec4(u_colour,1);
+    color = vec4(u_colour,1);
 }
 
--- src/comp3170/week2/vertex.glsl
+++ src/comp3170/week2/vertex.glsl
@@ -1,5 +1,6 @@
+#version 410
 
-attribute vec2 a_position;	/* vertex position as a 2D vector */
+in vec2 a_position;	/* vertex position as a 2D vector */
 
 void main() {
 	/* Pad the vertex to a 4D vector */
